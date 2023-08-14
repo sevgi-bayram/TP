bc43e943c933463c6786eac71b4ab79434f4813b
sevgi-bayram <127026476+sevgi-bayram@users.noreply.github.com>
2023-08-14 15:59:02 +0200

    determiner_le_jour_de_la_semaine.c
    Programme complet : Demander à l’utilisateur une date entière et lui donner le jour de la semaine correspondant.

--- determiner_le_jour_de_la_semaine.c
+++ determiner_le_jour_de_la_semaine.c
@@ -1,13 +1,64 @@
 #include <stdio.h>
 
-int	main(void)
+int    main(void)
 {
-    unsigned jour;
+    unsigned    jour;
+    unsigned    mois;
+    unsigned    an;
 
-    printf("Entrez un jour : ");
-    scanf("%u", &jour);
+    printf("Entrez une date (jj/mm/aaaa) : ");
+    scanf("%u/%u/%u", &jour, &mois, &an);
 
-    switch ((jour - 1) % 7)
+    unsigned njours = (an - 1) * 365;
+
+    if (an > 1582)
+    {
+        njours += ((an - 1) / 4);
+        njours -= ((an - 1) / 100);
+        njours += ((an - 1) / 400);
+        njours += 2;
+    }
+    else /* Calendrier Julien */
+        njours += ((an - 1) / 4);
+
+    njours += (mois - 1) * 31;
+
+    switch (mois)
+    {
+    case 12:
+        --njours;
+    case 11:
+    case 10:
+        --njours;
+    case 9:
+    case 8:
+    case 7:
+        --njours;
+    case 6:
+    case 5:
+        --njours;
+    case 4:
+    case 3:
+        if (an > 1582)
+        {
+            if (an % 4 == 0 && (an % 100 != 0 || an % 400 == 0))
+                njours -= 2;
+            else
+                njours -= 3;
+        }
+        else
+        {
+            if (an % 4 == 0)
+                njours -= 2;
+            else
+                njours -= 3;
+        }
+        break;
+    }
+    
+    njours += (jour - 1);
+
+    switch (njours % 7)
     {
     case 0:
         printf("C'est un samedi\n");
